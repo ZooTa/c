5726de49bf0ca666082ca7e789969c47bb4fc54c
Yousif Ahmed <65381604+ZooTa@users.noreply.github.com>
2021-09-09 04:33:04 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("how much dollars do you have ?");
-    double pound  = 15.67; //This Price for 8/11/2020
-    double dollar;
-    scanf("%lf", &dollar);
-    double result = pound * dollar;
-    printf("you have %.2lf pound", result);
+int main()
+{
+    int first=8;
+    int second=8;
+    int sum;
+    FILE *p;
+
+    p = fopen("zxc.txt","a+");
+
+    fseek(p , 0 , SEEK_SET);
+    fscanf(p,"%d",&first);
+
+    fseek(p , 2 , SEEK_SET);
+    fscanf(p,"%d",&second);
+
+    sum = first + second;
+
+    fprintf(p ,"%d" , sum);
+
+    fclose(p);
+
+    printf("%d\n", first);
+    printf("%d\n", second);
+    printf("%d", sum);
+
     return 0;
-    }
+}
