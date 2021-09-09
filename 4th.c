72ee671b945c429f6202755563e781dc4bdebe72
Yousif Ahmed <65381604+ZooTa@users.noreply.github.com>
2021-09-09 04:33:30 +0200

    Add files via upload

---
+++ 4th.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int first=8;
+    int second=8;
+    int sum;
+    FILE *p;
+
+    p = fopen("zxc.txt","a+");
+
+    fseek(p , 0 , SEEK_SET);
+    fscanf(p,"%d",&first);
+
+    fseek(p , 2 , SEEK_SET);
+    fscanf(p,"%d",&second);
+
+    sum = first + second;
+
+    fprintf(p ,"%d" , sum);
+
+    fclose(p);
+
+    printf("%d\n", first);
+    printf("%d\n", second);
+    printf("%d", sum);
+
+    return 0;
+}
